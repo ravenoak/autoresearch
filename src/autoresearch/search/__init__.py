3f49d71805f1e15e622cab7b656c70ed2585b45e
Caitlyn O'Hanna <caitlyn.ohanna@gmail.com>
2025-07-26 08:52:00 -0700

    Export spacy availability in search package

--- src/autoresearch/search/__init__.py
+++ src/autoresearch/search/__init__.py
@@ -3,10 +3,10 @@
 from .core import Search
 from .context import (
     SearchContext,
+    spacy,
     SPACY_AVAILABLE,
     BERTOPIC_AVAILABLE,
     SENTENCE_TRANSFORMERS_AVAILABLE,
-    spacy,
 )
 from .http import get_http_session, set_http_session, close_http_session, _http_session
 from ..config import get_config
@@ -19,8 +19,8 @@ __all__ = [
     "close_http_session",
     "_http_session",
     "get_config",
-    "SPACY_AVAILABLE",
     "BERTOPIC_AVAILABLE",
     "SENTENCE_TRANSFORMERS_AVAILABLE",
     "spacy",
+    "SPACY_AVAILABLE",
 ]
