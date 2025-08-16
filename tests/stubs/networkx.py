357863087e345a07f98befbf59f8304831b883a5
Caitlyn O'Hanna <caitlyn.ohanna@gmail.com>
2025-08-15 22:14:05 -0700

    Restore example env and isolate config resources (#839)

--- tests/stubs/networkx.py
+++ tests/stubs/networkx.py
@@ -7,7 +7,16 @@ if "networkx" not in sys.modules:
     nx_stub = types.ModuleType("networkx")
 
     class Graph:
-        pass
+        def __init__(self, *args, **kwargs):
+            """Minimal graph stub accepting any arguments."""
+            pass
+
+    class DiGraph(Graph):
+        """Minimal directed graph stub."""
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
 
     nx_stub.Graph = Graph
+    nx_stub.DiGraph = DiGraph
     sys.modules["networkx"] = nx_stub
