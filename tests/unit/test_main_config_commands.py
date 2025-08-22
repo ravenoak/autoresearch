3c4a09909ddb9cf1173d29dafafd790eef20da7d
Caitlyn O'Hanna <caitlyn.ohanna@gmail.com>
2025-08-21 20:09:30 -0700

    test: import required modules for config CLI tests

--- tests/unit/test_main_config_commands.py
+++ tests/unit/test_main_config_commands.py
@@ -1,7 +1,7 @@
 import shutil
-from importlib import resources as importlib_resources
 from unittest.mock import MagicMock, patch
 
+import importlib_resources
 import pytest
 from typer.testing import CliRunner
 
