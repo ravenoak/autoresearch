b9b4da6b1535128d5e946b6996d1420a6485a7ff
Caitlyn O'Hanna <caitlyn.ohanna@gmail.com>
2025-09-03 11:01:54 -0700

    test: cover extras markers

---
+++ tests/unit/test_extras_markers.py
@@ -0,0 +1,41 @@
+import pytest
+
+
+@pytest.mark.requires_nlp
+def test_nlp_marker():
+    assert True
+
+
+@pytest.mark.requires_ui
+def test_ui_marker():
+    assert True
+
+
+@pytest.mark.requires_vss
+def test_vss_marker():
+    assert True
+
+
+@pytest.mark.requires_git
+def test_git_marker():
+    assert True
+
+
+@pytest.mark.requires_distributed
+def test_distributed_marker():
+    assert True
+
+
+@pytest.mark.requires_analysis
+def test_analysis_marker():
+    assert True
+
+
+@pytest.mark.requires_llm
+def test_llm_marker():
+    assert True
+
+
+@pytest.mark.requires_parsers
+def test_parsers_marker():
+    assert True
